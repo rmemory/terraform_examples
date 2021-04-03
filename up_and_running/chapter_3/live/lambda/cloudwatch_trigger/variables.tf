6beac009de0f6e49e51784196595b5ed64a43450
Richard Memory <richardfmemory@gmail.com>
2021-04-03 12:07:08 -0600

    Move contents of chapter_3 into it's own repo

--- up_and_running/chapter_3/live/lambda/cloudwatch_trigger/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "emails" {
-  description = "List of emails to subscribe"
-  type = list(string)
-}
-
-variable "sms_phone_numbers" {
-  description = "List of phone numbers to send SMS messages. Must use format like this +1 206 555 1010."
-  type = list(string)
-}
